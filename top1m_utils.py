ae17a426270f79423120f1d265ab81f7011197ca
Tingfeng <wu.tingfeng@u.nus.edu>
2021-12-05 12:05:38 +0800

    Remove urllib

--- top1m_utils.py
+++ top1m_utils.py
@@ -1,18 +1,17 @@
 from __future__ import annotations
 from io import BytesIO
 from zipfile import ZipFile
-from urllib.request import urlopen
-from urllib.error import HTTPError
+import requests
 import logging
 
 def get_top1m_whitelist() -> list[str]:
     """Downloads the Tranco TOP1M Whitelist and returns all whitelisted URLs."""
     logging.info("Downloading TOP1M list...")
     try:
-      resp = urlopen("https://tranco-list.eu/top-1m.csv.zip")
-      zipfile = ZipFile(BytesIO(resp.read()))
+      resp = requests.get("https://tranco-list.eu/top-1m.csv.zip")
+      zipfile = ZipFile(BytesIO(resp.content))
       top1m_urls = [x.strip().decode().split(',')[1] for x in zipfile.open(zipfile.namelist()[0]).readlines()]
       logging.info("Downloading TOP1M list... [DONE]")
       return top1m_urls
-    except HTTPError as e:
+    except requests.exceptions.RequestException as e:
       raise SystemExit(e)
