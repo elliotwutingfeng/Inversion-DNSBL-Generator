622af86c856d327842226d235f3b72803a171ea6
Tingfeng <wu.tingfeng@u.nus.edu>
2021-12-16 22:45:47 +0800

    Add date and timestamp to logging messages

--- logger_utils.py
+++ logger_utils.py
@@ -1,10 +1,16 @@
 import logging
 
 def init_logger():
+    # Add date and timestamp to logging messages
+    logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%d-%m-%Y %H:%M:%S')
+
     # Disables filelock logging clutter from tldextract library
     logging.getLogger("filelock").setLevel(logging.WARNING)
     
     logger = logging.getLogger()
-    logger.setLevel(logging.INFO)
-    
+    #logger.setLevel(logging.INFO)
+
     return logger
