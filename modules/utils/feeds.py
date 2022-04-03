2b19f3ed347924a8d3b42ee6f5c10662106498b4
Tingfeng <wu.tingfeng@u.nus.edu>
2022-04-03 12:05:22 +0800

    modularise generate_hostname_expressions

--- modules/utils/feeds.py
+++ modules/utils/feeds.py
@@ -13,8 +13,48 @@ logger = init_logger()
 hostname_expression_batch_size: int = 40_000
 
 
+def generate_hostname_expressions_(raw_url: str) -> list[str]:
+    """Generate Safe Browsing API-compliant hostname expressions
+    for a given `raw_url`
+
+    Args:
+        raw_url (str): URL, without prefixes
+        (e.g. http:// https:// etc.),
+        to generate Safe Browsing API-compliant
+        hostname expression from.
+
+    Returns:
+        list[str]: Safe Browsing API-compliant
+        hostname expressions for `raw_url`. If `raw_url` is invalid,
+        return empty list.
+    """
+    # Remove zero width spaces from raw_url
+    url = re.sub(r"[\u200B-\u200D\uFEFF]", "", raw_url)
+    try:
+        ext = tldextract.extract(url)
+        if ext.registered_domain == "":
+            # No registered_domain recognised -> do not
+            # split url into parts
+            parts = []
+        elif ext.subdomain == "":
+            # No subdomains found -> extract registered_domain
+            parts = [ext.registered_domain]
+        else:
+            # Subdomains and registered_domain found -> extract them all
+            parts = ext.subdomain.split(".") + [ext.registered_domain]
+
+        # Safe Browsing API-compliant hostname expressions
+        # Include [url] for cases where url has a subdirectory
+        # (e.g. google.com/<subdirectory>)
+        return [f"{'.'.join(parts[-i:])}" for i in range(len(parts) if len(parts) < 5 else 5)] + [url]
+    except Exception as error:
+        logger.error("%s %s", url, error, exc_info=True)
+        return []
+
+
 def generate_hostname_expressions(raw_urls: list[str]) -> set[str]:
     """Generate Safe Browsing API-compliant hostname expressions
+    for a given batch of `raw_urls`
 
     See
     https://developers.google.com/safe-browsing/v4/urls-hashing#suffixprefix-expressions
@@ -26,31 +66,12 @@ def generate_hostname_expressions(raw_urls: list[str]) -> set[str]:
         hostname expressions from.
 
     Returns:
-        set[str]: `raw_urls` + Safe Browsing API-compliant
-        hostname expressions of `raw_urls`
+        set[str]: Safe Browsing API-compliant
+        hostname expressions for each url in `raw_urls`
     """
 
     hostname_expressions = set()
     for raw_url in raw_urls:
-        # Remove zero width spaces from raw_url
-        url = re.sub(r"[\u200B-\u200D\uFEFF]", "", raw_url)
-        try:
-            ext = tldextract.extract(url)
-            if ext.registered_domain == "":
-                # No registered_domain recognised -> do not
-                # split url into parts
-                parts = []
-            elif ext.subdomain == "":
-                # No subdomains found -> extract registered_domain
-                parts = [ext.registered_domain]
-            else:
-                # Subdomains and registered_domain found -> extract them all
-                parts = ext.subdomain.split(".") + [ext.registered_domain]
+        hostname_expressions.update(generate_hostname_expressions_(raw_url))
 
-            # Safe Browsing API-compliant hostname expressions
-            # Include [url] for cases where url has a subdirectory
-            # (e.g. google.com/<subdirectory>)
-            hostname_expressions.update([f"{'.'.join(parts[-i:])}" for i in range(len(parts) if len(parts) < 5 else 5)] + [url])
-        except Exception as error:
-            logger.error("%s %s", url, error, exc_info=True)
     return hostname_expressions
