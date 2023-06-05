5abee4e4141b66a07c2c4cf8453eaf2753ded4f6
Wu Tingfeng <wutingfeng@outlook.com>
2023-06-05 10:51:17 +0800

    Use CTE

--- modules/database/select.py
+++ modules/database/select.py
@@ -4,6 +4,7 @@ SQLite utilities for making SELECT queries
 import os
 
 from apsw import Error
+
 from modules.database.common import vacuum_and_close
 from modules.database.connect import create_connection
 from modules.utils.log import init_logger
@@ -13,9 +14,7 @@ from modules.utils.types import Vendors
 logger = init_logger()
 
 
-async def retrieve_matching_hash_prefix_urls(
-    db_filename: str, prefix_sizes: list[int], vendor: Vendors
-) -> list[str]:
+async def retrieve_matching_hash_prefix_urls(db_filename: str, prefix_sizes: list[int], vendor: Vendors) -> list[str]:
     """Identify URLs from `db_filename`.db database with sha256 hashes beginning with
     any of the malicious URL hash prefixes in `malicious`.db database.
 
@@ -34,9 +33,7 @@ async def retrieve_matching_hash_prefix_urls(
         try:
             cur = conn.cursor()
             with conn:
-                cur = cur.execute(
-                    f"ATTACH database 'databases{os.sep}malicious.db' as malicious"
-                )
+                cur = cur.execute(f"ATTACH database 'databases{os.sep}malicious.db' as malicious")
                 cur = cur.execute(
                     """
                     CREATE TEMPORARY TABLE IF NOT EXISTS vendorHashPrefixes
@@ -70,9 +67,7 @@ async def retrieve_matching_hash_prefix_urls(
     return urls
 
 
-async def retrieve_matching_full_hash_urls(
-    update_time: int, db_filename: str, vendor: Vendors
-) -> list[str]:
+async def retrieve_matching_full_hash_urls(update_time: int, db_filename: str, vendor: Vendors) -> list[str]:
     """Identify URLs from `db_filename`.db database with sha256 hashes matching with
     any of the malicious URL full hashes in `malicious`.db database, and updates
     malicious status of URL in `db_filename`.db database
@@ -89,12 +84,20 @@ async def retrieve_matching_full_hash_urls(
     """
     vendor_to_update_query = {
         "Google": """
+        WITH vendorFullHashes AS (
+        SELECT fullHash FROM malicious.maliciousFullHashes
+                    WHERE vendor = 'Google'
+        )
                     UPDATE urls
                     SET lastGoogleMalicious = ?
                     WHERE hash IN vendorFullHashes
                     RETURNING url
                     """,
         "Yandex": """
+        WITH vendorFullHashes AS (
+        SELECT fullHash FROM malicious.maliciousFullHashes
+                    WHERE vendor = 'Yandex'
+        )
                     UPDATE urls
                     SET lastYandexMalicious = ?
                     WHERE hash IN vendorFullHashes
@@ -109,20 +112,9 @@ async def retrieve_matching_full_hash_urls(
         try:
             cur = conn.cursor()
             with conn:
-                cur = cur.execute(
-                    f"ATTACH database 'databases{os.sep}malicious.db' as malicious"
-                )
-                cur = cur.execute(
-                    """
-                    CREATE TEMPORARY TABLE IF NOT EXISTS vendorFullHashes
-                    AS SELECT fullHash FROM malicious.maliciousFullHashes
-                    WHERE vendor = ?
-                    """,
-                    (vendor,),
-                )
+                cur = cur.execute(f"ATTACH database 'databases{os.sep}malicious.db' as malicious")
                 cur = cur.execute(vendor_to_update_query[vendor], (update_time,))
-                urls += [x[0] for x in cur.fetchall()]
-                cur.execute("DROP TABLE vendorFullHashes")
+                urls = [x[0] for x in cur.fetchall()]
         except Error as error:
             logger.error(
                 "filename:%s vendor:%s %s",
@@ -177,14 +169,11 @@ def retrieve_malicious_urls(urls_db_filenames: list[str], vendor: Vendors) -> li
         list[str]: URLs deemed by Safe Browsing API of `vendor` to be malicious
     """
     logger.info(
-        "Retrieving URLs from database most recently "
-        "marked as malicious by %s Safe Browsing API",
+        "Retrieving URLs from database most recently " "marked as malicious by %s Safe Browsing API",
         vendor,
     )
 
-    async def retrieve_malicious_urls_(
-        urls_db_filename: str, vendor: Vendors
-    ) -> set[str]:
+    async def retrieve_malicious_urls_(urls_db_filename: str, vendor: Vendors) -> set[str]:
         malicious_urls: set[str] = set()
         conn = create_connection(urls_db_filename)
         if conn is not None:
@@ -229,8 +218,7 @@ def retrieve_malicious_urls(urls_db_filenames: list[str], vendor: Vendors) -> li
         )
     )
     logger.info(
-        "Retrieving URLs from database most recently"
-        " marked as malicious by %s Safe Browsing API...[DONE]",
+        "Retrieving URLs from database most recently" " marked as malicious by %s Safe Browsing API...[DONE]",
         vendor,
     )
     return list(malicious_urls)
